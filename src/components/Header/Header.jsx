8f8c152479b1e0feb6683d03558b4871fd9997b6
aniketsingh10 <aniketsinghofficial144@gmail.com>
2024-02-19 11:17:24 +0530

    fixed bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -67,14 +67,14 @@ export function Header() {
       >
         <Link to="/portfolio">Portfolio</Link>
       </Typography>
-      <Typography
+      {/* <Typography
         as="li"
         variant="small"
         color="blue-gray"
         className="p-1 font-normal hover:scale-105 duration-300 ease-in-out"
       >
         <Link to="/dashboard">Dashboard</Link>
-      </Typography>
+      </Typography> */}
       <Typography
         as="li"
         variant="small"
